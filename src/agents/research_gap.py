a8c67364e2e69797ad73f506628a0a6411e0b91a
Inika15 <inikarao15@gmail.com>
2026-09-12 12:51:44 +0530

    Add ResearchGap and ResearchGapAnalysis models

--- src/agents/research_gap.py
+++ src/agents/research_gap.py
@@ -1,1 +1,22 @@
+from pydantic import BaseModel, Field
+from typing import List
+import os
+import json
+import google.generativeai as genai
 
+class ResearchGap(BaseModel):
+  gap_id: str = Field(description = "unique code")
+  title : str = Field(description = "Title of the paper")
+  category : str = Field(description = "Category of the gap")
+  description : str = Field(description = "More on the content")
+  affected_papers : List[str] = Field(default=[], description = "List of affected papers")
+
+class ResearchGapAnalysis(BaseModel):
+  explicit_gaps: List[ResearchGap] = Field(default=[], description = "List of author stated limitations")
+  synthesized_gaps: List[ResearchGap] = Field(default=[], description = "List of cross paper synthesized research gaps")
+  
+def run_research_gap_agent(schemas):
+  paper_context = ""
+  for paper in schemas:
+    paper_context += f"\nPaper Title: {paper.paper_title}\nLimitations: {paper.explicit_limitations}\n"
+    
